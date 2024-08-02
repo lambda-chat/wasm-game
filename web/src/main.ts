45975e979a6c6b5b9c478eab2cbbfa64fb606328
lambda-chat <chat.algebrique@gmail.com>
2024-08-03 04:05:50 +0900

    impl sierpinski

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,27 +1,5 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import init, { greet } from '../pkg'
+import init, { main_js } from "../pkg/wasm_puzzle.js";
 
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button class="greet" type="button">Greet</button> <!-- Added greet button -->
-    </div>
-  </div>
-`
-
-init();
-
-const greetButton = document.querySelector<HTMLButtonElement>('.greet')!;
-greetButton.addEventListener('click', () => {
-  greet()
-});
+init().then(() => {
+  main_js();
+}).catch(console.error);
